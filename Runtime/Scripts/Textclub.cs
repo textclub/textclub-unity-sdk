de4c165437fc3f39ad4499cf8547ba1c0b9ba217
Yasen Atanasov <y.atanasoff@gmail.com>
2025-03-20 17:30:08 +0200

    updated textclub with notifications and analytics

--- Runtime/Scripts/Textclub.cs
+++ Runtime/Scripts/Textclub.cs
@@ -5,6 +5,10 @@ namespace Textclub
 {
     public sealed class Textclub
     {
+        public readonly Notifications notifications = new();
+
+        public readonly Analytics analytics = new();
+
         public readonly Player player = new();
     }
 }
