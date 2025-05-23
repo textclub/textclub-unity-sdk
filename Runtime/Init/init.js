f75a08391f1168939b660afaf9366435fcecb10a
Petar Dobrev <peter.dobrev@gmail.com>
2025-05-23 18:14:44 +0300

    SDK: Auto initializing debug mode if not run on Textclub.com (#3)

--- Runtime/Init/init.js
+++ Runtime/Init/init.js
@@ -1,31 +1,32 @@
-
 function marshalString(value) {
-    var bufferSize = lengthBytesUTF8(value) + 1;
-    var buffer = _malloc(bufferSize);
-    stringToUTF8(value, buffer, bufferSize);
-    return buffer;
+  var bufferSize = lengthBytesUTF8(value) + 1;
+  var buffer = _malloc(bufferSize);
+  stringToUTF8(value, buffer, bufferSize);
+  return buffer;
 }
 
 async function initializeSdkAsync() {
-    let module = await import("https://textclub.github.io/textclub/pre-releases/latest/index.js");
-    window.textclubSdk = new module.Sdk();
-    await textclubSdk.isReady();
-    console.log("Initialized.");
+  const { textclub } = await import(
+    "https://textclub.github.io/textclub/pre-releases/latest/textclub.esm.js"
+  );
+  window.textclubSdk = textclub;
+  await window.textclubSdk.init();
+  console.log("Initialized.");
 }
 
 async function bootstrapSdkAsync(initializeSdkAsync) {
-    try {
-        // Do not start engine main() until we have downloaded & initialized the SDK
-        Module.addRunDependency('initializeSdkAsync');
-        await initializeSdkAsync();
-    } catch (e) {
-        console.error(`Unable to initialize SDK - ${e.message}`);
-    } finally {
-        Module.removeRunDependency('initializeSdkAsync');
-    }
+  try {
+    // Do not start engine main() until we have downloaded & initialized the SDK
+    Module.addRunDependency("initializeSdkAsync");
+    await initializeSdkAsync();
+  } catch (e) {
+    console.error(`Unable to initialize SDK - ${e.message}`);
+  } finally {
+    Module.removeRunDependency("initializeSdkAsync");
+  }
 }
 
 // Only run this code in the main thread
 if (!Module.ENVIRONMENT_IS_PTHREAD) {
-    Module.preRun.push(() => bootstrapSdkAsync(initializeSdkAsync));
+  Module.preRun.push(() => bootstrapSdkAsync(initializeSdkAsync));
 }
